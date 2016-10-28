4c0c9ac387051a49e970746450076b4e269eb1ef
Thomas Klijnsma <klthomas@student.ethz.ch>
2016-10-28 14:50:56 +0200

    script to run fits as jobs updated for cross platform, more options, etc

--- Plotting/jobs/runFitJobs.py
+++ Plotting/jobs/runFitJobs.py
@@ -25,7 +25,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument( '--test', action='store_true', help='Does not submit the job, but creates the .sh file and prints')
 parser.add_argument( '-q', '--queue',
     type=str, choices=['short', 'all', 'long', '8nm', '1nh', '8nh', '1nd', '2nd', '1nw', '2nw' ],
-    default='short', help='which queue to submit to')
+    default='1nh', help='which queue to submit to')
 parser.add_argument( '-n', '--normalmemory', action='store_true', help='By default more memory is requested; this option disables that')
 parser.add_argument( '-k', '--keep', action='store_true', help='Does not clean the output and jobscript directories')
 args = parser.parse_args()
@@ -75,25 +75,114 @@ def main():
     if onPsi and not os.path.isdir(stdDir): os.makedirs( stdDir )
             
 
-    WSs = [
-        # '../savedResults/Config_Sep30_electron_EB_ECALonly_results.root',
-        '../savedResults/Config_Sep30_electron_EB_ECALTRK_results.root',
-        # '../savedResults/Config_Sep30_electron_EE_ECALonly_results.root',
-        '../savedResults/Config_Sep30_electron_EE_ECALTRK_results.root',
-        # '../savedResults/Config_Sep30_photon_EB_ECALonly_results.root',
-        # '../savedResults/Config_Sep30_photon_EE_ECALonly_results.root',
+    ########################################
+    # Arrange input to Fit.py
+    ########################################
+
+    # EB and EE ECALonly training
+    # result = '../savedResults/UsedForOct28Talk/Config_Oct20_electron_EB_ECALonly_results.root'
+    # result = '../savedResults/UsedForOct28Talk/Config_Oct20_electron_EE_ECALonly_results.root'
+    
+    # EB TRK training
+    # result = '../savedResults/UsedForOct28Talk/Config_Oct25_electron_EB_TRKstep2_FastOptions_results.root'
+    # result = '../savedResults/UsedForOct28Talk/Config_Oct25_electron_EB_TRKstep2_SlowOptions_results.root'
+
+    # EE TRK training
+    # result = '../savedResults/UsedForOct28Talk/Config_Oct26_electron_EE_TRKstep2_FastOptions_results.root'
+    # result = '../savedResults/UsedForOct28Talk/Config_Oct26_electron_EE_TRKstep2_results.root'
+
+    # globalPtBins = [
+    #     0.,
+    #     100.,
+    #     500.,
+    #     2500.,
+    #     6500.,
+    #     ]
+
+    # ntup = '../applyRegression/Config_Oct20_electron_EB_ECALonly_results_testing_ptWeight.root'
+    # ntup = '../applyRegression/Config_Oct20_electron_EB_ECALonly_results_testing_sample_ptWeight.root'
+    
+    # ntup = '../applyRegression/Config_Oct20_electron_EE_ECALonly_results_testing_ptWeight.root'
+    # ntup = '../applyRegression/Config_Oct20_electron_EE_ECALonly_results_testing_sample_ptWeight.root'
+
+    fitjobs = [
+
+        # Format:
+        # (
+        #     'path/to/result.root',
+        #     list of pt bins, eg: [0., 100., 500., 2500., 6500., ],
+        #     'path/tp/ntup.root',
+        #     True/False regarding whether result is a "second regression" result
+        #     ),
+
+        ### ELECTRONS
+
+        # # EB fitjob
+        # (
+        #     '../savedResults/UsedForOct28Talk/Config_Oct25_electron_EB_TRKstep2_FastOptions_results.root',
+        #     [0., 100., 500., 2500., 6500., ],
+        #     '../applyRegression/Config_Oct20_electron_EB_ECALonly_results_testing_ptWeight.root',
+        #     True
+        #     ),
+
+
+        # # EE fitjob
+        # (
+        #     '../savedResults/UsedForOct28Talk/Config_Oct26_electron_EE_TRKstep2_FastOptions_results.root',
+        #     [0., 100., 500., 2500., 6500., ],
+        #     '../applyRegression/Config_Oct20_electron_EE_ECALonly_results_testing_ptWeight.root',
+        #     True
+        #     ),
+
+
+        ### PHOTONS
+
+        # EB fitjob
+        (
+            '../savedResults/UsedForOct28Talk/Config_Oct26_photon_EB_ECALonly_NI3000_results.root',
+            [0., 100., 500., 2500., 6500., ],
+            '/afs/cern.ch/work/t/tklijnsm/public/CMSSW_8_0_4/src/NTuples/Ntup_Jul22_fullpt_testing.root',
+            False
+            ),
+
+        # EE fitjob
+        (
+            # '../savedResults/UsedForOct28Talk/Config_Oct26_photon_EB_ECALonly_NI3000_results.root',
+            # '../Config_Oct27_photon_EE_ECALonly_NI3000_Fast_results.root',
+            '../Config_Oct27_photon_EE_ECALonly_NI3000_results.root',
+            [0., 100., 500., 2500., 6500., ],
+            '/afs/cern.ch/work/t/tklijnsm/public/CMSSW_8_0_4/src/NTuples/Ntup_Jul22_fullpt_testing.root',
+            False
+            ),
+
         ]
 
 
+    ########################################
+    # Make command and submit
+    ########################################
 
-    for WS in WSs:
+    for result, globalPtBins, ntup, isSecondRegression in fitjobs:
 
-        Make_jobscript( WS, jobscriptDir, stdDir )
+        for iPtBound in xrange( len(globalPtBins)-1 ):
 
-        if WS != WSs[-1] and not args.test:
-            nSleep = 2
-            print 'Sleeping {0} seconds to prevent jobs from interfering with one another'.format(nSleep)
-            sleep(nSleep)
+            ptMin = int(globalPtBins[iPtBound])
+            ptMax = int(globalPtBins[iPtBound+1])
+
+            cmd = 'python Fit.py {0} --ntup {1} --ptbins {2} {3}'.format(
+                result, ntup, ptMin, ptMax )
+
+            if isSecondRegression:
+                cmd += ' --second-regression'
+
+            shFileName = os.path.basename(result).replace('.root','') + '_PT{0:04d}-{1:04d}.sh'.format( int(ptMin), int(ptMax) )
+
+            Make_jobscript( cmd, shFileName, jobscriptDir, stdDir )
+
+            if iPtBound != len(globalPtBins)-2 and not args.test:
+                nSleep = 1
+                print 'Sleeping {0} seconds to prevent jobs from interfering with one another'.format(nSleep)
+                sleep(nSleep)
 
 
 
@@ -101,12 +190,12 @@ def main():
 # Functions
 ########################################
 
-def Make_jobscript( WS, jobscriptDir, stdDir ):
+def Make_jobscript( fitcmd, resultName, jobscriptDir, stdDir ):
 
     # ======================================
     # Creating the sh file
 
-    sh_file = jobscriptDir + '/run_' + (os.path.basename(WS)).replace( '.root', '.sh' )
+    sh_file = jobscriptDir + '/run_' + (os.path.basename(resultName)).replace( '.root', '.sh' )
 
     sh_fp = open( sh_file, 'w' )
     p = lambda text: sh_fp.write( text + '\n' )
@@ -129,7 +218,7 @@ def Make_jobscript( WS, jobscriptDir, stdDir ):
     p( 'echo "START OF FITTING"' )
     p( '#' + '-'*50 )
 
-    p( 'python Fit.py ' + WS )
+    p( fitcmd )
 
     p( '#' + '-'*50 )
     p( 'echo "END OF FITTING"' )
@@ -169,8 +258,8 @@ def Make_jobscript( WS, jobscriptDir, stdDir ):
 
     if args.test:
         print 'TEST MODE. Would now run: ' + cmd
+        print '          which executes: ' + fitcmd
     else:
-        # os.system( 'bsub -q 2nw ' + sh_file )
         os.system( cmd )
         # print 'Script disabled'
 
