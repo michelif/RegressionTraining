0e19003c9434d51c739b259ab249a94e86f15f74
Thomas Klijnsma <klthomas@student.ethz.ch>
2016-10-28 15:25:57 +0200

    Commands that execute applyRegressionWrapper.py as a job

---
+++ applyRegression/exampleCommand.sh
@@ -0,0 +1,25 @@
+TRAINING_NTUP="/afs/cern.ch/work/t/tklijnsm/public/CMSSW_8_0_4/src/NTuples/Ntup_Jul22_fullpt_training.root"
+TESTING_NTUP="/afs/cern.ch/work/t/tklijnsm/public/CMSSW_8_0_4/src/NTuples/Ntup_Jul22_fullpt_testing.root"
+TESTING_SAMPLE_NTUP="/afs/cern.ch/work/t/tklijnsm/public/CMSSW_8_0_4/src/NTuples/Ntup_Jul22_fullpt_testing_sample.root"
+
+RESULT="../savedResults/UsedForOct28Talk/Config_Oct20_electron_EE_ECALonly_results.root"
+
+python applyRegressionWrapper.py \
+    --config ../python/Config_Oct20_electron_EE_ECALonly.config \
+    --result "$RESULT" \
+    --ntup   "$TESTING_SAMPLE_NTUP" \
+    --batch
+
+python applyRegressionWrapper.py \
+    --config ../python/Config_Oct20_electron_EE_ECALonly.config \
+    --result "$RESULT" \
+    --ntup   "$TRAINING_NTUP" \
+    --keep \
+    --batch
+
+python applyRegressionWrapper.py \
+    --config ../python/Config_Oct20_electron_EE_ECALonly.config \
+    --result "$RESULT" \
+    --ntup   "$TESTING_NTUP" \
+    --keep \
+    --batch
