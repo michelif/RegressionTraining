af9995b644e9545cd3e613a3958a4af7c98a9b59
Thomas Klijnsma <klthomas@student.ethz.ch>
2016-11-25 12:08:14 +0100

    command used to run the ecal regression on the ntuples

--- applyRegression/exampleCommand.sh
+++ applyRegression/exampleCommand.sh
@@ -1,25 +1,41 @@
+# TRAINING_NTUP="/afs/cern.ch/work/t/tklijnsm/public/CMSSW_8_0_4/src/NTuples/Ntup_Jul22_fullpt_training.root"
+# TESTING_NTUP="/afs/cern.ch/work/t/tklijnsm/public/CMSSW_8_0_4/src/NTuples/Ntup_Jul22_fullpt_testing.root"
+# TESTING_SAMPLE_NTUP="/afs/cern.ch/work/t/tklijnsm/public/CMSSW_8_0_4/src/NTuples/Ntup_Jul22_fullpt_testing_sample.root"
+
+# RESULT="../savedResults/UsedForOct28Talk/Config_Oct20_electron_EE_ECALonly_results.root"
+
+# python applyRegressionWrapper.py \
+#     --config ../python/Config_Oct20_electron_EE_ECALonly.config \
+#     --result "$RESULT" \
+#     --ntup   "$TESTING_SAMPLE_NTUP" \
+#     --batch
+
+# python applyRegressionWrapper.py \
+#     --config ../python/Config_Oct20_electron_EE_ECALonly.config \
+#     --result "$RESULT" \
+#     --ntup   "$TRAINING_NTUP" \
+#     --keep \
+#     --batch
+
+# python applyRegressionWrapper.py \
+#     --config ../python/Config_Oct20_electron_EE_ECALonly.config \
+#     --result "$RESULT" \
+#     --ntup   "$TESTING_NTUP" \
+#     --keep \
+#     --batch
+
+
+# Remade with run and lumi numers included
+
 TRAINING_NTUP="/afs/cern.ch/work/t/tklijnsm/public/CMSSW_8_0_4/src/NTuples/Ntup_Jul22_fullpt_training.root"
 TESTING_NTUP="/afs/cern.ch/work/t/tklijnsm/public/CMSSW_8_0_4/src/NTuples/Ntup_Jul22_fullpt_testing.root"
 TESTING_SAMPLE_NTUP="/afs/cern.ch/work/t/tklijnsm/public/CMSSW_8_0_4/src/NTuples/Ntup_Jul22_fullpt_testing_sample.root"
 
-RESULT="../savedResults/UsedForOct28Talk/Config_Oct20_electron_EE_ECALonly_results.root"
+RESULT="../savedResults/UsedForOct28Talk/Config_Oct20_electron_EB_ECALonly_results.root"
 
 python applyRegressionWrapper.py \
-    --config ../python/Config_Oct20_electron_EE_ECALonly.config \
+    --config ../savedResults/UsedForOct28Talk/Configs/Config_Oct20_electron_EB_ECALonly.config \
     --result "$RESULT" \
     --ntup   "$TESTING_SAMPLE_NTUP" \
-    --batch
-
-python applyRegressionWrapper.py \
-    --config ../python/Config_Oct20_electron_EE_ECALonly.config \
-    --result "$RESULT" \
-    --ntup   "$TRAINING_NTUP" \
-    --keep \
-    --batch
-
-python applyRegressionWrapper.py \
-    --config ../python/Config_Oct20_electron_EE_ECALonly.config \
-    --result "$RESULT" \
-    --ntup   "$TESTING_NTUP" \
-    --keep \
-    --batch
+    --out "Ntup_Nov04_runLumi.root"
+    # --batch
