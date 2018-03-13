e2b4899817bc52354e58d318968739684e60d7d3
Francesco <micheli@cern.ch>
2018-03-13 16:21:45 +0100

    small fix

--- applyRegression/convertRootToHd5.py
+++ applyRegression/convertRootToHd5.py
@@ -3,8 +3,10 @@ import numpy as np
 import pandas as pd
 import root_pandas as rpd
 
-df = rpd.read_root('ntuples_NN/Ntup_10Nov_Photon_testing.root','een_analyzer/correction')
+name='Ntup_10Nov_Photon_testing_allvar.root'
+
+df = rpd.read_root('ntuples_NN/'+name,'een_analyzer/correction')
 
 df.head()
 
-df.to_hdf('ntuples_NN/Ntup_10Nov_Photon_testing.hd5','photonTree',compression=9,complib='bzip2',mode='w',format='table',data_columns=['NtupID'])
+df.to_hdf('ntuples_NN/'+name.replace('.root','.hd5'),'photonTree',compression=9,complib='bzip2',mode='w',format='table',data_columns=['NtupID'])
